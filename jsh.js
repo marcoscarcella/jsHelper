4c57804df565c1c0bb88185b5b8b3c8db4467739
Marco <33720121+marcoscarcella@users.noreply.github.com>
2020-09-22 12:37:04 +0200

    Update jsh.js

--- jsh.js
+++ jsh.js
@@ -1,3 +1,4 @@
+//intercept all ajax calls
 const intercept = (urlmatch, callback) => {
     let send = XMLHttpRequest.prototype.send;
     XMLHttpRequest.prototype.send = function() {
@@ -25,3 +26,29 @@ intercept('https://api3.youpic.com/user/1028252/fan/to', function(x) {
     console.dir(Object.keys(users2).length);
 
 });
+
+//Monitor div change
+var monitor = function(selectorContainer, selectorElement, f){
+    var targetNode = document.querySelector(selectorContainer);
+    var config = { attributes: true, childList: true, subtree: true };
+    var callback = function(mutationsList, observer) {
+        if(mutationsList[0].addedNodes){
+            let el = mutationsList[0].addedNodes[0];
+            if(el.id == selectorElement){
+                f();
+            }
+        }
+    };
+    var observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+    //observer.disconnect();
+}
+
+monitor('#container', 'test2', function(){console.log('OKOKOK')});
+
+let test1 = document.createElement('div');
+test1.id="test1";
+let test2 = document.createElement('div');
+test2.id="test2";
+setTimeout(()=>document.getElementById('container').appendChild(test1), 2001);
+setTimeout(()=>document.getElementById('container').appendChild(test2), 2000);
